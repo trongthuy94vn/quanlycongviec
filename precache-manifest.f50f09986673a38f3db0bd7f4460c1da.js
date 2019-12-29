9b49052cf87a6fa69c4c445a19346b9d9803cde2
trongthuy94vn <vutrongthuy94hd@gmail.com>
2019-12-29 22:00:09 +0700

    Updates

---
+++ precache-manifest.f50f09986673a38f3db0bd7f4460c1da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd78ca0284536fc0a02277265764cd49",
+    "url": "/quanlycongviec/index.html"
+  },
+  {
+    "revision": "eb3b864d9e8e4db86646",
+    "url": "/quanlycongviec/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "c5e43cfdfd75cf9cebb1",
+    "url": "/quanlycongviec/static/css/main.5df7d988.chunk.css"
+  },
+  {
+    "revision": "eb3b864d9e8e4db86646",
+    "url": "/quanlycongviec/static/js/2.90a6b7ca.chunk.js"
+  },
+  {
+    "revision": "b15a737ad2cfd16685fd27e8e85315d2",
+    "url": "/quanlycongviec/static/js/2.90a6b7ca.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c5e43cfdfd75cf9cebb1",
+    "url": "/quanlycongviec/static/js/main.e6f14b1a.chunk.js"
+  },
+  {
+    "revision": "e1c53140377bb7e9e7d1",
+    "url": "/quanlycongviec/static/js/runtime-main.2746ce4c.js"
+  }
+]);
